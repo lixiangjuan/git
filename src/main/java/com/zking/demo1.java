1ee2972133e90e8161d25506735eb40c16811d9a
lixiangjuan <1367352320@qq.com>
2020-11-26 11:17:57 +0800

    Initial commit

---
+++ src/main/java/com/zking/demo1.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2020-2020, XXX有限公司
+ * FileName: demo1
+ * Author:   zjjt
+ * Date:     2020/11/26 11:15
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.zking;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author zjjt
+ * @create 2020/11/26
+ * @since 1.0.0
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.printf("hello ");
+    }
+}
